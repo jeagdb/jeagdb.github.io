c8044b8b06ed17ffcfb547e432e7e0cf33d8af6d
picou75 <jeanne.gardebois@gmail.com>
2024-11-05 17:35:24 +0100

    chatbot optimisation
    add defer attribute set to true

--- src/components/Chatbot/index.js
+++ src/components/Chatbot/index.js
@@ -3,21 +3,26 @@ import { useEffect } from 'react'
 const Chatbot = ({ }) => {
   useEffect(() => {
     (function (g, e, n, es, ys) {
-      g['_genesysJs'] = e;
+      g['_genesysJs'] = e
       g[e] = g[e] || function () {
         (g[e].q = g[e].q || []).push(arguments)
-      };
-      g[e].t = 1 * new Date();
-      g[e].c = es;
-      ys = document.createElement('script');
-      ys.async = 1;
-      ys.src = n;
-      ys.charset = 'utf-8';
-      document.head.appendChild(ys);
+      }
+      g[e].t = 1 * new Date()
+      g[e].c = es
+      ys = document.createElement('script')
+      ys.async = 1
+      ys.src = n
+      ys.defer = true
+      ys.charset = 'utf-8'
+      document.head.appendChild(ys)
     })(window, 'Genesys', 'https://apps.mypurecloud.ie/genesys-bootstrap/genesys.min.js', {
       environment: 'prod-euw1',
       deploymentId: 'e34cdf1e-853e-4352-9470-31b012d46adf'
     })
+
+    // Retarde le chargement de 3 secondes
+    // const timer = setTimeout(loadChatbot, 3000)
+    // return () => clearTimeout(timer)
   }, [])
 
   return null
