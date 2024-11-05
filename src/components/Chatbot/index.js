ceeb2579d0dd08ddde7f5a84acbb9db787be02aa
picou75 <jeanne.gardebois@gmail.com>
2024-11-05 17:12:50 +0100

    chatbot seo check
    check SEO impact with genesys chatbot

---
+++ src/components/Chatbot/index.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react'
+
+const Chatbot = ({ }) => {
+  useEffect(() => {
+    (function (g, e, n, es, ys) {
+      g['_genesysJs'] = e;
+      g[e] = g[e] || function () {
+        (g[e].q = g[e].q || []).push(arguments)
+      };
+      g[e].t = 1 * new Date();
+      g[e].c = es;
+      ys = document.createElement('script');
+      ys.async = 1;
+      ys.src = n;
+      ys.charset = 'utf-8';
+      document.head.appendChild(ys);
+    })(window, 'Genesys', 'https://apps.mypurecloud.ie/genesys-bootstrap/genesys.min.js', {
+      environment: 'prod-euw1',
+      deploymentId: 'e34cdf1e-853e-4352-9470-31b012d46adf'
+    })
+  }, [])
+
+  return null
+}
+
+export default Chatbot
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import SocketProvider from '../config/SocketProvider'
+import Chatbot from '@/components/Chatbot'
 
 const App = ({ Component, pageProps }) => {
   return (
     <SocketProvider>
       <Component {...pageProps} />
+      <Chatbot />
     </SocketProvider>
   )
 }
