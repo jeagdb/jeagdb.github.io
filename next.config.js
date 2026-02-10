ceed8086750a78b4fadf477fd96e6e38baa90f1f
jeagdb <jeanne.gardebois@gmail.com>
2026-02-10 22:22:13 +0100

    update dependancies

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withFonts = require('next-fonts')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
     unoptimized: true
